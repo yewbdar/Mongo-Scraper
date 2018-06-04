4c51408428cc3cc77eacbcda525e940bc57244d9
Yewbdar <Yewbdar.grima@gmail.com>
2018-06-04 13:45:49 -0500

    added route for add and remove articles

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,43 +1,74 @@
-$(document).on("click",".add-article",function(){
-     var id=$(this).val();
-     var article={}
-     article.title=$("."+id).children().children(".link").text();
-     article.link=$("."+id).children().children(".link").attr("href");
-     article.summry=$("."+id).children(".summary").text();
+$(document).on("click", ".add-article", function () {
+    var id = $(this).val();
+    var article = {}
+    article.title = $("." + id).children().children(".link").text();
+    article.link = $("." + id).children().children(".link").attr("href");
+    article.summry = $("." + id).children(".summary").text();
 
-     
-     $.ajax({
+
+    $.ajax({
         method: "POST",
         url: "/addarticles",
         data: article
-        
-      }).then(function(data) {
-          console.log("data saved ")
+
+    }).then(function (data) {
+        console.log("data saved ")
         window.location.reload();
 
-        });
+    });
 })
 $.ajax({
     method: "GET",
     url: "/"
-  }).then(function(data) {
-      console.log(data);
+}).then(function (data) {
+    console.log(data);
 
-    });
+});
 
-$(document).on("click", ".remove-article", function() {
+$(document).on("click", ".remove-article", function () {
     // Grab the id associated with the article from the submit button
     var thisId = $(this).val();
-  
+
     // Run a POST request to change the note, using what's entered in the inputs
     $.ajax({
-      method: "POST",
-      url: "/rmvarticles/" + thisId,
-      
-    })
-      .then(function(data) {
-        window.location.reload();
-        
-      });
+        method: "POST",
+        url: "/rmvarticles/" + thisId,
 
+    })
+        .then(function (data) {
+            window.location.reload();
+
+        });
+
+});
+$(document).on("click", ".add-note", function () {
+    var id = $(this).val();
+    $.ajax({
+        method: "GET",
+        url: "/note/" + id
+    }).then(function (data) {
+        console.log(data);
+         $(".saved-note").text(data.note.body)
+         $(".remove").val(data.note._id);
     });
+    
+    $(".save").val(id);
+   $(".modal-title").text("Note for Article :"+id); 
+    $(".modal").modal('show');
+       
+})
+
+$(document).on("click",".save",function(req,res){
+   var id=$(this).val();
+   console.log(id);
+   $.ajax({
+       method:"POST",
+       url:"/addnote/"+id,
+       data:{
+        body:$(".note").val()
+       }
+   }).then(function (data) {
+    window.location.reload();
+
+});
+})
